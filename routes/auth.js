dc7f821f66adcff09f3a7ee5626e75f27ba63663
an nguyen <hello.an.ng@gmail.com>
2017-01-30 16:37:55 -0800

    saves auth redirect draft

--- routes/auth.js
+++ routes/auth.js
@@ -17,24 +17,33 @@ router.get('/login', (req, res, next) => {
 });
 
 router.get('/', (req, res, next) => {
+  console.log('req.query.code', req.query.code);
   const code = req.query.code;
   const data = {
     client_id: process.env.SLACK_CLIENT_ID,
     client_secret: process.env.SLACK_CLIENT_SECRET,
     code: code,
     redirect_uri: redirect_uri
+    // state: req.query.state
   }
+  console.log('this is data', data);
   const options = {
     method: 'POST',
     url: 'https://slack.com/api/oauth.access',
-    headers: { 'Accept': 'application/json'},
+    headers: {
+     // { 'Accept': 'application/json'},
+      'content-type': 'application/json; charset=utf-8'
+    },
+    // contentType: 'application/json',
+    // contentType: 'application/json;charset=ISO-8859-15',
     json: data
   }
   request(options, (err, response, body) => {
-    console.log(response);
+    // console.log(response);
     if (!err && response.statusCode === 200) {
       req.session.access_token = body.access_token;
       // res.redirect('/profile');
+      // console.log(response);
       console.log(body);
       res.send(body);
     }
@@ -42,7 +51,7 @@ router.get('/', (req, res, next) => {
     //   console.log(body);
     //   res.send(body);
     // }
-  })
+  });
 });
 
 
