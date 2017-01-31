3a3d732f391d037662457c9e0c23489c4dc9eccf
an nguyen <hello.an.ng@gmail.com>
2017-01-30 18:08:46 -0800

    renders user name and email

--- routes/auth.js
+++ routes/auth.js
@@ -9,24 +9,22 @@ const redirect_uri = 'http://127.0.0.1:3000/auth';
 router.get('/login', (req, res, next) => {
   const client_id = process.env.SLACK_CLIENT_ID;
   const url = 'https://slack.com/oauth/authorize';
-  const scope = 'users:read';
+  const scope = 'users.profile:read';
   const state = 'meow';
-  // const team = 'the-wanderers';
   const queryParams = `client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}&state=${state}`;
   res.redirect(url + '?' + queryParams);
 });
 
 router.get('/', (req, res, next) => {
-  console.log('req.query.code', req.query.code);
+  // console.log('req.query.code', req.query.code);
   const code = req.query.code;
   const data = {
     client_id: process.env.SLACK_CLIENT_ID,
     client_secret: process.env.SLACK_CLIENT_SECRET,
     code: code,
     redirect_uri: redirect_uri
-    // state: req.query.state
   }
-  console.log('this is data', data);
+  // console.log('this is data', data);
   var options = {
     method: 'GET',
     uri: 'https://slack.com/api/oauth.access?code=' + code + '&client_id=' + process.env.SLACK_CLIENT_ID + '&client_secret=' + process.env.SLACK_CLIENT_SECRET + '&redirect_uri=' + redirect_uri
@@ -34,16 +32,13 @@ router.get('/', (req, res, next) => {
   request(options, (err, response, body) => {
     // console.log(response);
     if (!err && response.statusCode === 200) {
-      req.session.access_token = body.access_token;
-      // res.redirect('/profile');
+      const data = JSON.parse(body);
+      req.session.access_token = data.access_token;
       // console.log(response);
       console.log(body);
-      res.send(body);
+      // res.send(JSON.parse(body));
+      res.redirect('/profile');
     }
-    // else {
-    //   console.log(body);
-    //   res.send(body);
-    // }
   });
 });
 
--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request');
 
 router.get('/', (req, res, next) => {
-  res.render('profile');
+  const access_token = req.session.access_token;
+  console.log('ACCESS TOKEN ', access_token);
+  const url = `https://slack.com/api/users.profile.get?token=${access_token}&pretty=1`;
+  const options = {
+    method: 'GET',
+    url: url
+  }
+  request(options, (err, response, body) => {
+    // if (!err && response.statusCode === 200) {
+      const user = JSON.parse(body);
+      console.log(user);
+      res.render('profile', {user: user.profile});
+    // }
+  });
 });
 
 module.exports = router;
